50a27b20acccdc599178c60ecd083bb9ac4dd99d
Brandon Williams <b_williams20@mail.fhsu.edu>
2025-05-14 08:56:12 -0400

    Force HTML for header on error handling attempt and fixed spacing

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 
 // Connect to MongoDB
 mongoose.connect(process.env.DATABASE_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
@@ -21,13 +21,14 @@ app.use('/states', require('./routes/states'));
 
 // Set header...
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.send('<h1>Final API - States Data</h1>');
+    res.setHeader('Content-Type', 'text/html');
+    res.send('<h1>Final API - States Data</h1>');
 });
 
 // Standard error handler...
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>404 Not Found</h1>');
+    res.setHeader('Content-Type', 'text/html');
+    res.status(404).send('<h1>404 Not Found</h1>');
 });
 
 // Start...
